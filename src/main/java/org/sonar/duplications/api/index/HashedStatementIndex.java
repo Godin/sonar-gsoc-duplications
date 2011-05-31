d4c360b408c01d8c3cbe88150716cba1c7aca15a
Andrew <tereskin@gmail.com>
2011-05-31 14:34:51 +0400

    clone index access interface and tuple added with test

---
+++ src/main/java/org/sonar/duplications/api/index/HashedStatementIndex.java
@@ -0,0 +1,70 @@
+/*
+ * Sonar, open source software quality management tool.
+ * Written (W) 2011 Andrew Tereskin
+ * Copyright (C) 2008-2011 SonarSource
+ * mailto:contact AT sonarsource DOT com
+ *
+ * Sonar is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Sonar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sonar; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.duplications.api.index;
+
+import java.util.Set;
+import java.util.SortedSet;
+
+public interface HashedStatementIndex {
+    /**
+     * Method performs search in index db. Parameter is full filename inside
+     * project exactly the same as it was saved in db.
+     * If file is moved inside project it should be removed and then added again.
+     *
+     * @param fileName - full path of file in project with filename
+     * @return list of <tt>HashedTuple</tt> from index and empty list if nothing found
+     */
+    public SortedSet<HashedTuple> getByFilename(String fileName);
+
+    /**
+     * Search by sequence hash.
+     *
+     * @param sequenceHash - hash of statement sequence
+     * @return set of <tt>HashedTuple</tt> from index and empty list if nothing found
+     */
+    public Set<HashedTuple> getBySequenceHash(byte[] sequenceHash);
+
+    public void insert(HashedTuple tuple);
+
+    /**
+     * Remove all <tt>HashedTuple</tt> from index with <tt>fileName</tt>
+     *
+     * @param fileName - full path of file in project with filename
+     */
+    public void remove(String fileName);
+
+    /**
+     * Remove all elements from index that are equals to <tt>tuple</tt>
+     *
+     * @param tuple - tuple to be removed from index
+     */
+    public void remove(HashedTuple tuple);
+
+    /**
+     * Empty hash index - remove all tuples
+     */
+    public void removeAll();
+
+    /**
+     * Total number of tuples in index
+     */
+    public int size();
+}
