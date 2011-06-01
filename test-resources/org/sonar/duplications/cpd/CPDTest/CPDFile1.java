8929d57eceacc94121c2d218cbe93b0fc80320d4
suddin <auni_ku@yahoo.com>
2011-06-01 13:15:30 -0600

    added a new location for test resources

---
+++ test-resources/org/sonar/duplications/cpd/CPDTest/CPDFile1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class  DirListing{
+		public static void main(String[] args){
+		switch(args.length){
+			case 0: System.out.println("Directory has not mentioned.");
+					System.exit(0);
+			case 1: dirlist(args[0]);
+					System.exit(0);
+			default : System.out.println("Multiple files are not allow.");
+					  System.exit(0);
+		}
+	}
+}
