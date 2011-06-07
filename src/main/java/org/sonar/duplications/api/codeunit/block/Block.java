7708239207ef80359453c08b392ddd391c9af2f7
Andrew <tereskin@gmail.com>
2011-06-07 15:14:00 +0400

    fix hashCode and equals for byte[] + renames

--- src/main/java/org/sonar/duplications/api/codeunit/block/Block.java
+++ src/main/java/org/sonar/duplications/api/codeunit/block/Block.java
@@ -1,65 +1,78 @@
 package org.sonar.duplications.api.codeunit.block;
 
+import org.sonar.duplications.util.BlockUtils;
+
+import java.util.Arrays;
+
 /**
  * @author sharif
- *
  */
 public class Block {
 
-	private final String originId; //filename
+  private final String resourceId; //filename
 
-	private final byte[] blockHash;
+  private final byte[] blockHash;
 
-	private final int firstUnitIndex;
+  private final int firstUnitIndex;
 
-	private final int firstLineNumber;
+  private final int firstLineNumber;
 
-	private final int lastLineNumber;
+  private final int lastLineNumber;
 
-	public Block(String originId, byte[] blockHash,
-			int firstUnitIndex, int firstLineNumber, int lastLineNumber) {
-		this.originId = originId;
-		this.blockHash = blockHash;
-		this.firstUnitIndex = firstUnitIndex;
-		this.firstLineNumber = firstLineNumber;
-		this.lastLineNumber = lastLineNumber;
-	}
+  private final int arrHashCode;
 
-	public String getOriginId() {
-		return originId;
-	}
+  public Block(String resourceId, byte[] blockHash,
+               int firstUnitIndex, int firstLineNumber, int lastLineNumber) {
+    this.resourceId = resourceId;
+    this.blockHash = blockHash;
+    this.firstUnitIndex = firstUnitIndex;
+    this.firstLineNumber = firstLineNumber;
+    this.lastLineNumber = lastLineNumber;
+    this.arrHashCode = Arrays.hashCode(blockHash);
+  }
 
-	public byte[] getChunkHash() {
-		return blockHash;
-	}
+  public String getResourceId() {
+    return resourceId;
+  }
 
-	public int getFirstUnitIndex() {
-		return firstUnitIndex;
-	}
+  public byte[] getBlockHash() {
+    return blockHash;
+  }
 
-	public int getFirstLineNumber() {
-		return firstLineNumber;
-	}
+  public int getFirstUnitIndex() {
+    return firstUnitIndex;
+  }
 
-	public int getLastLineNumber() {
-		return lastLineNumber;
-	}
+  public int getFirstLineNumber() {
+    return firstLineNumber;
+  }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (!(obj instanceof Block)) {
-			return false;
-		}
-		Block other = (Block) obj;
-		return other.originId.equals(originId)
-				&& other.blockHash == blockHash
-				&& other.firstUnitIndex == firstUnitIndex
-				&& other.firstLineNumber == firstLineNumber
-				&& other.lastLineNumber == lastLineNumber;
-	}
+  public int getLastLineNumber() {
+    return lastLineNumber;
+  }
 
-	@Override
-	public int hashCode() {
-		return originId.hashCode() + blockHash.hashCode() + 413 * firstUnitIndex;
-	}
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Block)) {
+      return false;
+    }
+    Block other = (Block) obj;
+    return other.resourceId.equals(resourceId)
+        && Arrays.equals(other.blockHash, blockHash)
+        && other.firstUnitIndex == firstUnitIndex
+        && other.firstLineNumber == firstLineNumber
+        && other.lastLineNumber == lastLineNumber;
+  }
+
+  @Override
+  public int hashCode() {
+    return resourceId.hashCode() + arrHashCode + 413 * firstUnitIndex;
+  }
+
+  @Override
+  public String toString() {
+    return "'" + resourceId + "'[" + firstUnitIndex + "|" + firstLineNumber
+        + "-" + lastLineNumber + "]:" + BlockUtils.getHex(blockHash);
+  }
+
 }
