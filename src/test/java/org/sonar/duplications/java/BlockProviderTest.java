907df43a378f6038d48b2234a9e4d609ab9a57f6
Andrew <tereskin@gmail.com>
2011-06-07 15:16:28 +0400

    clean comment and redundant cast warning

--- src/test/java/org/sonar/duplications/java/BlockProviderTest.java
+++ src/test/java/org/sonar/duplications/java/BlockProviderTest.java
@@ -1,12 +1,6 @@
 package org.sonar.duplications.java;
 
-import java.io.File;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.List;
-
 import junit.framework.Assert;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.sonar.duplications.api.codeunit.block.Block;
@@ -18,44 +12,48 @@ import org.sonar.duplications.api.lexer.Lexer;
 import org.sonar.duplications.api.lexer.LexterFactory;
 import org.sonar.duplications.api.sourcecode.SourceCodeElement;
 
+import java.io.File;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BlockProviderTest {
-	
-	BlockProvider blockProvider;
-	
-	File testFile = new File("test-resources/org/sonar/duplications/cpd/CPDTest/CPDFile1.java");
 
-	@Before
-	public void initTest(){
-		
-		SourceCodeElement rootSourceElement = new SourceCodeElement(testFile.getPath() , Charset.defaultCharset(), ELanguage.JAVA);
-		Lexer lexer = LexterFactory.getJavaLexer();
-		TokenProvider tokenProvider = new TokenProvider(lexer);
-		StatementProvider statementProvider = new StatementProvider(tokenProvider, true);
-		blockProvider = new BlockProvider(statementProvider, 3);
-		blockProvider.init(rootSourceElement);
-	}
-	
-	@Test
-	public void shouldTokenizeSource(){
-		List <Block> blockList = new ArrayList<Block>();
-		Block block;
-		while( (block = blockProvider.getNext() ) != null){
-			blockList.add((Block) block);
-			//System.out.println(block.getChunkHash().toString());
-		}
-		
-		Assert.assertEquals(0, blockList.get(0).getFirstUnitIndex());
-		Assert.assertEquals(1, blockList.get(0).getFirstLineNumber());
-		Assert.assertEquals(4, blockList.get(0).getLastLineNumber());
+  BlockProvider blockProvider;
 
-		Assert.assertEquals(7, blockList.get(blockList.size()-1).getFirstUnitIndex());
-		Assert.assertEquals(9, blockList.get(blockList.size()-1).getFirstLineNumber());
-		Assert.assertEquals(11, blockList.get(blockList.size()-1).getLastLineNumber());
-		
+  File testFile = new File("test-resources/org/sonar/duplications/cpd/CPDTest/CPDFile1.java");
+
+  @Before
+  public void initTest() {
+
+    SourceCodeElement rootSourceElement = new SourceCodeElement(testFile.getPath(), Charset.defaultCharset(), ELanguage.JAVA);
+    Lexer lexer = LexterFactory.getJavaLexer();
+    TokenProvider tokenProvider = new TokenProvider(lexer);
+    StatementProvider statementProvider = new StatementProvider(tokenProvider, true);
+    blockProvider = new BlockProvider(statementProvider, 3);
+    blockProvider.init(rootSourceElement);
+  }
+
+  @Test
+  public void shouldTokenizeSource() {
+    List<Block> blockList = new ArrayList<Block>();
+    Block block;
+    while ((block = blockProvider.getNext()) != null) {
+      blockList.add(block);
+    }
+
+    Assert.assertEquals(0, blockList.get(0).getFirstUnitIndex());
+    Assert.assertEquals(1, blockList.get(0).getFirstLineNumber());
+    Assert.assertEquals(4, blockList.get(0).getLastLineNumber());
+
+    Assert.assertEquals(7, blockList.get(blockList.size() - 1).getFirstUnitIndex());
+    Assert.assertEquals(9, blockList.get(blockList.size() - 1).getFirstLineNumber());
+    Assert.assertEquals(11, blockList.get(blockList.size() - 1).getLastLineNumber());
+
 //		assertThat(blockList, hasItems(
 //				new Block(filename, null, 0 , 1 , 4), 
 //				new Block(filename, null, 7 , 9 , 11)));
-		
-	}
-	
+
+  }
+
 }
