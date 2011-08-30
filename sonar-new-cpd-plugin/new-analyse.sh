ecac51890d745a2bfd069654a91794c459f10ba4
Evgeny Mandrikov <mandrikov@gmail.com>
2011-08-30 18:24:11 +0400

    Remove sonar-new-cpd-plugin since code was merged into sonar-cpd-plugin

--- sonar-new-cpd-plugin/new-analyse.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Analyse with new plugin
-mvn sonar:sonar -Dsonar.dynamicAnalysis=false -Dsonar.skipDesign=true -Dsonar.branch=NEW -Dsonar.cpd.skip=true
--- sonar-new-cpd-plugin/old-analyse.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Analyse with old plugin
-mvn sonar:sonar -Dsonar.dynamicAnalysis=false -Dsonar.skipDesign=true -Dsonar.branch=OLD -Dsonar.newcpd.skip=true
